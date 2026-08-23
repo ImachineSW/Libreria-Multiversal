e424c8bd9065c927a1d05e48435b5b9fd99b7322
maximodn <fornoto68@gmail.com>
2026-08-23 20:44:25 -0300

    4C: funcion eliminar libro

--- js/carrito.js
+++ js/carrito.js
@@ -8,6 +8,7 @@ function mostrarCarrito() {
     let total = 0; // inicializa la variable
 
     cont.innerHTML = ""; // reinicia la pagina
+    bookr.innerHTML = ""; // reinicia el resumen de libros
 
     if (carrito.length === 0) { // si el carrito esta vacio muestra esto
         cont.innerHTML = `<h3>CARRITO VACIO</h3>`;
@@ -31,11 +32,24 @@ function mostrarCarrito() {
         `; // por cada libro en el array agrega eso
 
         cont.appendChild(div); // agrega los div a la pagina
+
+        const boton = div.querySelector(".eliminar"); // busca el boton de los libros
+        boton.addEventListener("click", () => eliminar(libro)); // a los botones le da la funcion de eliminar el libro
     });
 
     resumen.textContent = `Total: $${total}`; // actualiza el total
 
 };
+//
+
+// funcion para eliminar un libro
+function eliminar(libro) { 
+    let carrito = cargarCarrito(); // carga el carrito guardado
+    carrito = carrito.filter(item => item.id !== libro.id); // elimina el libro del carrito
+    
+    guardarCarrito(carrito); // guarda el carrito luego de eliminar el libro
+    mostrarCarrito(); // vuelve a mostrar el carrito
+}
 //
 
 document.addEventListener("DOMContentLoaded", () => {
